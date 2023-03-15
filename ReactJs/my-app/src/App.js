2f516626bf7362c8b06d03b5d4e63a256b009785
indiakka <indiakka@gmail.com>
2023-03-15 16:47:03 +0100

    Hooks de React

--- ReactJs/my-app/src/App.js
+++ ReactJs/my-app/src/App.js
@@ -1,5 +1,9 @@
 import "./App.css";
-import ComponenteA from "./components/ejercicio 1,2,3/componenteB";
+//import ComponenteA from "./components/ejercicio 1,2,3/componenteB";
+//import Ejemplo1 from "./hooks/Ejemplo1";
+//import Ejemplo2 from "./hooks/Ejemplo2";
+//import MiComponenteConContexto from "./hooks/Ejemplo3";
+import Ejemplo4 from "./hooks/Ejemplo4";
 //import TaskListComponent from "./components/container/task_list";
 //import Greeting from "./components/pure/greeting";
 //import GreetingF from "./components/pure/greetingF";
@@ -14,7 +18,18 @@ function App() {
         {/* <GreetingF name={"Martin"} /> */}
         {/* Componente de listado de tareas */}
         {/* <TaskListComponent /> */}
-        <ComponenteA />
+        {/* <ComponenteA /> */}
+        {/* Ejemplos de uso de HOOKS */}
+        {/* <Ejemplo1 /> */}
+        {/* Ejemplos de uso de HOOKS */}
+        {/* <Ejemplo2 /> */}
+        {/* <MiComponenteConContexto /> */}
+        <Ejemplo4 nombre='Martin'>
+        {/* Todo lo que hay aquí, es trasladado como props.children */}
+          <h3>
+            Contenido del props.children
+          </h3>
+        </Ejemplo4>
       </header>
     </div>
   );
